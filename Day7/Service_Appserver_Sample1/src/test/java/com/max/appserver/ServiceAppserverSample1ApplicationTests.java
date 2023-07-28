cf5f5d2a00c498469d0da66c3725823fc6061ad5
deenadhayalan7 <113279582+deenadhayalan7@users.noreply.github.com>
2023-07-28 15:29:50 +0530

    Add files via upload

---
+++ Day7/Service_Appserver_Sample1/src/test/java/com/max/appserver/ServiceAppserverSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAppserverSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Day7/Service_Feedbackserver_Sample1/src/main/java/com/max/feedbackserver/ServiceFeedbackserverSample1Application.java
@@ -0,0 +1,13 @@
+package com.max.feedbackserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFeedbackserverSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceFeedbackserverSample1Application.class, args);
+	}
+
+}
